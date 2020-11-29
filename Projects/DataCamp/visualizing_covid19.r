cf91513b05937f9acadc20e4cf8a60d8570027fe
Carolina Almeida <carolinagalmeida.eng@gmail.com>
2020-11-29 13:12:37 -0300

    first commit

---
+++ Projects/DataCamp/visualizing_covid19.r
@@ -0,0 +1,10 @@
+# Load the readr, ggplot2, and dplyr packages
+library(readr)
+library(ggplot2)
+library(dplyr)
+
+# Read datasets/confirmed_cases_worldwide.csv into confirmed_cases_worldwide
+confirmed_cases_worldwide <- read_csv("datasets/confirmed_cases_worldwide.csv")
+
+# Print out confirmed_cases_worldwide
+confirmed_cases_worldwide
